d5b66a401cda5f110fc82b75b0760767c25ba9b8
Daire Finn <dmanfinn@gmail.com>
2025-02-17 23:18:27 +0000

    Implemented fragment arguments for Image width

--- newsfeed/src/components/Image.tsx
+++ newsfeed/src/components/Image.tsx
@@ -4,10 +4,24 @@ import { ImageFragment$key } from "./__generated__/ImageFragment.graphql";
 import { useFragment } from "react-relay";
 
 const ImageFragment = graphql`
-  fragment ImageFragment on Image {
-    url
-    altText
-  }
+  fragment ImageFragment on Image
+    @argumentDefinitions(
+      width: {
+        type: "Int",
+        defaultValue: null
+      },
+      height: {
+        type: "Int",
+        defaultValue: null
+      }
+    )
+    {
+      url(
+        width: $width
+        height: $height
+      )
+      altText
+    }
 `;
 
 type Props = {

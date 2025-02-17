1160a81861d3a6ab664fb6208d269a02bb0243c1
Daire Finn <dmanfinn@gmail.com>
2025-02-17 23:04:33 +0000

    Added a Query to get data and Fragments for the child component data

--- newsfeed/src/components/PosterByline.tsx
+++ newsfeed/src/components/PosterByline.tsx
@@ -1,28 +1,41 @@
 import * as React from "react";
 import Image from "./Image";
+import { graphql } from "relay-runtime";
+import { useFragment } from "react-relay";
+import type { PosterBylineFragment$key } from "./__generated__/PosterBylineFragment.graphql";
+
+const PosterByLineFragment = graphql`
+  fragment PosterBylineFragment on Actor {
+    name
+    profilePicture {
+      ...ImageFragment
+    }
+  }
+`;
 
 export type Props = {
-  poster: {
-    name: string;
-    profilePicture: {
-      url: string;
-    };
-  };
+  poster: PosterBylineFragment$key
 };
 
 export default function PosterByline({ poster }: Props): React.ReactElement {
   if (poster == null) {
     return null;
   }
+
+  const data = useFragment(
+    PosterByLineFragment,
+    poster
+  );
+
   return (
     <div className="byline">
       <Image
-        image={poster.profilePicture}
+        image={data.profilePicture}
         width={60}
         height={60}
         className="byline__image"
       />
-      <div className="byline__name">{poster.name}</div>
+      <div className="byline__name">{data.name}</div>
     </div>
   );
 }
